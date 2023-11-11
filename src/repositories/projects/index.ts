ec9006892d46285e52c72412befe6ee3f62ce90e
Jovanka Samudra <samudrajovanka20@gmail.com>
2023-11-11 18:51:00 +0700

    fix: make get project isr

--- src/repositories/projects/index.ts
+++ src/repositories/projects/index.ts
@@ -5,9 +5,9 @@ import type { GetProjectsResponse } from './types';
 export const getProjects = async () => {
   const data = await fetcher<GetProjectsResponse>({
     url: '/api/projects'
-    // next: {
-    //   revalidate: 60 * 60 * 24 * 7
-    // }
+    next: {
+      revalidate: 60 * 60 * 24 * 7
+    }
   });
 
   return data;
